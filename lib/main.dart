3aeb27ba39829056904f54a37f1acdddc1ab17da
Shreyas Nidugala <shreyasnidugala@gmail.com>
2022-07-07 15:32:46 +0530

    hot reload yay

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
         home: RandomWords());
@@ -20,27 +18,26 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
   Widget _buildList() {
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        Container(
-          height: 50,
-          color: Colors.amber[600],
-          child: const Center(child: Text('Entry A')),
-        ),
-        Container(
-          height: 50,
-          color: Colors.amber[500],
-          child: const Center(child: Text('Entry B')),
-        ),
-        Container(
-          height: 50,
-          color: Colors.amber[100],
-          child: const Center(child: Text('Entry C')),
-        ),
-      ],
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_randomWordPairs[index]);
+      },
     );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(title: Text("Hello"));
   }
 
   Widget build(BuildContext context) {
