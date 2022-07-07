314dbf1d29183e6f5f7752f77bbd552d875dad1d
Shreyas Nidugala <shreyasnidugala@gmail.com>
2022-07-07 13:04:30 +0530

    EnglishWords pkg and basic wordPair

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
         home: Scaffold(
-          appBar: AppBar(title: Text("Wordpair Generator")),
-          body: Center(child: Text('Hello World'))
-    ));
+            appBar: AppBar(title: Text("Wordpair Generator")),
+            body: Center(child: Text(wordPair.asPascalCase))));
   }
 }
